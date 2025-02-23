52a993849a5baa2ac251c0a6c36cf69f109ae8dd
shafee_azeem <shafeeazeemag@gmail.com>
2025-02-23 16:43:37 +0530

    updated CSS and JS

--- app.js
+++ app.js
@@ -4,19 +4,41 @@ const calendarDiv = document.querySelector(".calendar");
 const remainingDaysSpan = document.getElementById("remaining-day");
 
 function calculateWeeks(birthday) {
+    calendarDiv.innerHTML = "";
+
+
     const today = new Date();//today's date
     const birthTime = birthday.getTime();//convert birthday to milliseconds
     const currentTime = today.getTime();//convert current date
-
     const weeksLived = Math.floor((currentTime - birthTime) / (1000 * 60 * 60 * 24 * 7));
-
     const totalWeeks = 80 * 52;// assuming a person lives to be 80 years old
     const weeksRemaining = Math.max(totalWeeks - weeksLived, 0);
+
+    for (let i = 0; i < totalWeeks; i++) {
+        const weekBox = document.createElement("div");
+
+        if (i < weeksLived) {
+            weekBox.classList.add("past");
+            weekBox.classList.add("cross");
+            weekBox.textContent = "❌";
+        } else {
+            weekBox.classList.add("future");
+            weekBox.textContent = "✔️";
+        }
+
+        calendarDiv.appendChild(weekBox);
+    }
+
+    remainingDaysSpan.textContent = weeksRemaining * 7; // To display days remaining
+
     return { weeksLived, weeksRemaining };
+
 }
 
 generateButton.addEventListener("click", () => {
     const birthday = new Date(birthdayInput.value);
+    const birthdayValue = birthdayInput.value;
+    console.log("Selected birthday:", birthdayValue)
 
     if (!birthdayInput.value) {
         alert("Please select a valid birthdate");
